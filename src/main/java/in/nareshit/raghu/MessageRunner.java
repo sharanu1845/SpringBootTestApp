666cac66d33afbf5d3feaa3282602f3191d1f082
USER <USER@DESKTOP-JB3BFO7>
2023-04-02 14:06:38 +0530

    COMMIT NEXT TWO

--- src/main/java/in/nareshit/raghu/MessageRunner.java
+++ src/main/java/in/nareshit/raghu/MessageRunner.java
@@ -8,8 +8,8 @@ public class MessageRunner implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-     System.out.println("WELCOME TO SPRING BOOT APP");		
-     System.out.println("HELLO MODIFIED");	
+int a=10;
+System.out.println("HELLO MODIFIED");	
 
 	}
 
